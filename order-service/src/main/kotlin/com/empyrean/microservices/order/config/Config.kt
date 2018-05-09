22898a15fc2dde63a11ac68f29be49ab09814a7f
Chirdeep Tomar <chirdeep.tomar@gmail.com>
2018-05-09 16:45:30 +0100

    Webclient can be loadbalanced with Eureka

--- order-service/src/main/kotlin/com/empyrean/microservices/order/config/Config.kt
+++ order-service/src/main/kotlin/com/empyrean/microservices/order/config/Config.kt
@@ -3,17 +3,18 @@ package com.empyrean.microservices.order.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerExchangeFilterFunction
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
 
 @Configuration
-class RedisConfig {
+class ServiceConfig(private val lbFunction: LoadBalancerExchangeFilterFunction) {
 
     @Bean
     fun webClient(): WebClient {
-        return WebClient.create("http://192.168.2.82:8080/api/")
+        return WebClient.builder().filter(lbFunction).baseUrl("http://cart-micro-service/api/").build()
     }
 
 
